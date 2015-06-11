9b09f30c997a0795940a628a71b538955d87a287
Ian Birnam <ian3000@gmail.com>
2015-06-10 23:13:12 -0700

    toggle nav links when clicking logo, still need to toggle header background

---
+++ js/navToggleScript.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+  // var firstScroll = false;
+
+  function toggleNavigation(isNavClosed) {
+    $(".navItem").each(function(index) {
+      if (isNavClosed === true) {
+         $(this).css("opacity", 1.0);
+         $("#logoAnchor").removeClass("closed");
+      } else {
+         $(this).css("opacity", 0.0);
+         $("#logoAnchor").addClass("closed");
+      }
+    });
+    $(".navItem").each(function(index) {
+      if (isNavClosed === true) {
+         $(this).css("visibility", "visible");
+      } else {
+         $(this).css("visibility", "hidden");
+      }
+    });
+  };
+
+  $("#logoAnchor").click(function(evt) {
+    var navState = $("#logoAnchor").attr("class");
+    if (navState === "closed") {
+      toggleNavigation(true);
+    } else {
+      toggleNavigation(false);
+    }
+  });
+
+  // $(window).scroll(function() {
+  //   var scrollHeight = $(document).scrollTop();
+  //   if (scrollHeight === 0) {
+  //     toggleNavigation(false);
+  //     firstScroll = false;
+  //   } else {
+  //     if (firstScroll === false) {
+  //       toggleNavigation(true);
+  //       firstScroll = true;
+  //     }
+  //   }
+  // });
+});
