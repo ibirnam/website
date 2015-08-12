9cf2b4d0388792f970a3dea43569684cea7d51a5
Ian Birnam <ianb@vertigo.com>
2015-08-12 16:11:20 -0700

    fixed smooth scrolling to scroll precisely to headers

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -5,7 +5,8 @@ $(function() {
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
         $('html,body').animate({
-          scrollTop: target.offset().top
+          //added extra 100 offset to get scrolling to go exactly to headers
+          scrollTop: target.offset().top - 100
         }, 1000);
         return false;
       }
